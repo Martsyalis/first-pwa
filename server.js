28862c3509a04b6bb40bdd446e889639e62464a0
MaryusMartsyalis <martsyalis@gmail.com>
2018-01-22 16:45:36 -0800

    port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./lib/app');
 
 const server = http.createServer(app);
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
     // eslint-disable-next-line
