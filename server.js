f5458b14356515b0806261fce8f23abdbaf3249d
MaryusMartsyalis <martsyalis@gmail.com>
2018-01-22 16:21:12 -0800

    make server

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const app = require('./lib/app');
+
+const server = http.createServer(app);
+const port = process.env.port || 3000;
+
+server.listen(port, () => {
+    // eslint-disable-next-line
+    console.log('server running on', server.address().port);
+});
